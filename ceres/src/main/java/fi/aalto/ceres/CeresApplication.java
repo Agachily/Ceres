d103df9ddfa5db86dea750048d688a29ebec9c4b
Agachily <zetongzhao@foxmail.com>
2022-09-27 16:17:12 +0200

    Initialize project

---
+++ ceres/src/main/java/fi/aalto/ceres/CeresApplication.java
@@ -0,0 +1,13 @@
+package fi.aalto.ceres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeresApplication.class, args);
+	}
+
+}
